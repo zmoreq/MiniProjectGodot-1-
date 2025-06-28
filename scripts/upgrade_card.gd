595a48ec4ff9fa690ba965c3082a3510f313be04
zmoreq <91459732+zmoreq@users.noreply.github.com>
2025-06-28 11:50:57 +0200

    Started implementing modular upgrades

---
+++ scripts/upgrade_card.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
+	
+func set_data():
+	pass
---
+++ scripts/upgrades_data.gd
@@ -0,0 +1,27 @@
+extends Node
+
+var data = {
+	1 : {
+		"text" : "You have 20% more attack speed",
+		"stat" : "attack_speed",
+		"multiplier" : 1.2
+	},
+	2 : {
+		"text" : "You have 20% more damage",
+		"stat" : "damage",
+		"multiplier" : 1.2
+	},
+	3 : {
+		"text" : "You have 20% more movement speed",
+		"stat" : "movement_speed",
+		"multiplier" : 1.2
+	},
+	4 : {
+		"text" : "You have 50% more attack range",
+		"stat" : "attack_range",
+		"multiplier" : 1.5
+	}
+}
+
+func _ready() -> void:
+	pass
